2319dc709affd82f5b6779e5fb1f5f4b628c06ee
Shio <shio7113@gmail.com>
2020-09-01 21:51:23 +0900

    Create avatar.py

---
+++ cogs/avatar.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+class Avatar(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # Commands
+    @commands.command()
+    async def 아바타(self, ctx):
+        if (ctx.message.mentions.__len__() > 0):
+            for user in ctx.message.mentions:
+                pfp = str(user.avatar_url)
+                embed = discord.Embed(title="**" +user.name + "**님의 아바타", description="[Link]" + "(" + pfp + ")",
+                                      color=0xff0051)
+                embed.set_image(url=pfp)
+                embed.set_footer(text="Shio D Project", icon_url="https://raw.githubusercontent.com/Shio7/V11DX/master/images/a11.png")
+                await ctx.trigger_typing()
+                await ctx.send(embed=embed)
+        else:
+            pfp = ctx.author.avatar_url
+            embed = discord.Embed(title="**" + ctx.author.name + "**님의 아바타", description="[Link]" + "(" + str(pfp) + ")",
+                                color=0xff0051)
+            embed.set_image(url=pfp)
+            embed.set_footer(text="Shio D Project", icon_url="https://raw.githubusercontent.com/Shio7/V11DX/master/images/a11.png")
+            await ctx.trigger_typing()
+            await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(Avatar(client))
