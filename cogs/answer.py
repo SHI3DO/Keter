c2aafb7d7dd32611f5c7b036662df7cc4728a84b
Shio <shio7113@gmail.com>
2020-09-06 21:45:22 +0900

    s

--- cogs/answer.py
+++ cogs/answer.py
@@ -13,16 +13,18 @@ class Answer(commands.Cog):
         await ctx.trigger_typing()
         app_id = "VPR9G7-54PV53JYTK"
         client = wolframalpha.Client(app_id)
-        answer = next(res.results).text
-        embed = discord.Embed(title="Result", description=answer, color=0xeff0f1)
-        await ctx.send(embed=embed)
         try:
             res = client.query(content)
         except:
             embed = discord.Embed(title="IDK", description="I don't know about This bruuu.", color=0xeff0f1)
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/751791353779716099/751791533958627368/DARK_KETER.png")
+            embed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/751791353779716099/751791533958627368/DARK_KETER.png")
             await ctx.send(embed=embed)
             return
+        answer = next(res.results).text
+        embed = discord.Embed(title="Result", description=answer, color=0xeff0f1)
+        await ctx.send(embed=embed)
+
 
 def setup(client):
     client.add_cog(Answer(client))
