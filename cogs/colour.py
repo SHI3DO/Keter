17c79848f26978fe1072eecff2124a892e81cf86
Yeon <49362746+AngelusYeon@users.noreply.github.com>
2020-09-07 15:38:52 +0900

    Update colour.py

--- cogs/colour.py
+++ cogs/colour.py
@@ -39,6 +39,24 @@ class Colour(commands.Cog):
             await ctx.guild.edit_role_positions(positions=positions)
         embed = discord.Embed(title='**Import**', description='Completely imported whole roles.', colour=0xeff0f1)
         await ctx.send(embed=embed)
+        
+    @commands.command(aliases=['remove colour'])
+    @has_permissions(administrator=True, manage_messages=True, manage_roles=True)
+    async def remove_color(self, ctx):
+        """ Removes color roles. """
+        roles = ctx.guild.roles
+        position = 0
+        filename = "lib/color.xlsx"
+        book = openpyxl.load_workbook(filename)
+        sheet = book.active
+        colors = []
+        for i in range(3, 137):
+            colors.append(sheet["B" + str(i)].value)
+        for i in range(0, len(roles)):
+            if roles[i].name in colors:
+                ctx.guild.remove_roles(roles[i])
+        embed = discord.Embed(title='**Remove**', description='Completely Removed whole roles.', colour=0xeff0f1)
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
