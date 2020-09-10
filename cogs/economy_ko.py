59cc90a0aadf43e11334b6353299157fa5f4b2dd
Shio <61821471+Shio7@users.noreply.github.com>
2020-09-10 11:28:43 +0900

    Update economy_ko.py

--- cogs/economy_ko.py
+++ cogs/economy_ko.py
@@ -3,6 +3,8 @@ import time
 import psutil
 import os
 import asyncio
+import openpyxl
+import random
 
 from datetime import datetime
 from discord.ext import commands
@@ -15,6 +17,7 @@ class economy_ko(commands.Cog):
         self.config = default.get("config.json")
         self.process = psutil.Process(os.getpid())
 
+    #참여
     @commands.command()
     async def 참여(self, ctx):
 
@@ -37,9 +40,6 @@ class economy_ko(commands.Cog):
         try:
             await msg.add_reaction("✅")
             await self.bot.wait_for('raw_reaction_add', timeout=10.0, check=reaction_check_)
-            embed = discord.Embed(title="케테르 경제", description="케테르 경제에 참여하셨습니다.", color=0xeff0f1)
-            await ctx.send(embed=embed)
-
             if os.path.isfile(userlib + str(ctx.author.id) + ".xlsx"):
                 embed = discord.Embed(title="케테르 경제", description="이미 참여하셨습니다.", color=0xeff0f1)
                 await ctx.send(embed=embed)
@@ -48,6 +48,17 @@ class economy_ko(commands.Cog):
                                       color=0xeff0f1)
                 embed.set_thumbnail(
                     url="https://cdn.discordapp.com/attachments/750540820842807396/752690012369190942/DARK_KETER_1.png")
+                await ctx.send(embed=embed)
+                wb = openpyxl.Workbook()
+                ws = wb.active
+                ws.cell(row=1, column=1).value = "money"
+                ws.cell(row=1, column=2).value = "0"
+                wb.save(userlib + str(ctx.author.id) + ".xlsx")
+                wb.close()
+                time.sleep(1)
+                embed = discord.Embed(title="케테르 경제",
+                                      description=str(ctx.author.name) + " 생성 완료!",
+                                      color=0xeff0f1)
                 await ctx.send(embed=embed)
 
         except asyncio.TimeoutError:
@@ -62,6 +73,25 @@ class economy_ko(commands.Cog):
             embed.set_thumbnail(
                 url="https://cdn.discordapp.com/attachments/750540820842807396/752684853320745000/KETER_PRESTIGE.png")
             await msg.edit(content=embed)
+
+    @commands.command(aliases=['돈내놔', '돈줘'])
+    async def 돈받기(self,ctx):
+        if os.path.isfile(userlib + str(ctx.author.id) + ".xlsx"):
+            num = random.randrange(10,60)
+            wb = openpyxl.load_workbook(userlib + str(ctx.author.id) + ".xlsx")
+            ws = wb.active
+            getmoney = ws.cell(row=1, column=2).value
+            getmoney = int(getmoney) + int(num)
+            ws.cell(row=1, column=2).value = str(getmoney)
+            wb.save(userlib + str(ctx.author.id) + ".xlsx")
+            wb.close()
+            embed = discord.Embed(title="KTR", description=str(num)+"KTR을 받았어요!", color=0xeff0f1)
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(title="NO", description="먼저 ``.참여``를 입력해서 케테르 경제에 참여해주세요!", color=0xeff0f1)
+            embed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/750540820842807396/752684853320745000/KETER_PRESTIGE.png")
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
