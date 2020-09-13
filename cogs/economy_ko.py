721c5f52772a3cb60afef7ecbfded606d17dfb3b
Shio <61821471+Shio7@users.noreply.github.com>
2020-09-14 01:02:23 +0900

    ㄴ

--- cogs/economy_ko.py
+++ cogs/economy_ko.py
@@ -12,47 +12,6 @@ from discord.ext import commands
 from evs import default, permissions
 
 userlib = "./lib/economy/users/"
-
-
-def get_kor_amount_string(num_amount, ndigits_round=0, str_suffix='원'):
-    """숫자를 자릿수 한글단위와 함께 리턴한다 """
-    assert isinstance(num_amount, int) and isinstance(ndigits_round, int)
-    assert num_amount >= 1, '최소 1원 이상 입력되어야 합니다'
-    ## 일, 십, 백, 천, 만, 십, 백, 천, 억, ... 단위 리스트를 만든다.
-    maj_units = ['만', '억', '조', '경', '해', '자', '양', '구', '간', '정', '재', '극']  # 10000 단위
-    units = [' ']  # 시작은 일의자리로 공백으로하고 이후 십, 백, 천, 만...
-    for mm in maj_units:
-        units.extend(['십', '백', '천'])  # 중간 십,백,천 단위
-        units.append(mm)
-
-    list_amount = list(str(round(num_amount, ndigits_round)))  # 라운딩한 숫자를 리스트로 바꾼다
-    list_amount.reverse()  # 일, 십 순서로 읽기 위해 순서를 뒤집는다
-
-    str_result = ''  # 결과
-    num_len_list_amount = len(list_amount)
-
-    for i in range(num_len_list_amount):
-        str_num = list_amount[i]
-        # 만, 억, 조 단위에 천, 백, 십, 일이 모두 0000 일때는 생략
-        if num_len_list_amount >= 9 and i >= 4 and i % 4 == 0 and ''.join(list_amount[i:i + 4]) == '0000':
-            continue
-        if str_num == '0':  # 0일 때
-            if i % 4 == 0:  # 4번째자리일 때(만, 억, 조...)
-                str_result = units[i] + str_result  # 단위만 붙인다
-        elif str_num == '1':  # 1일 때
-            if i % 4 == 0:  # 4번째자리일 때(만, 억, 조...)
-                str_result = str_num + units[i] + str_result  # 숫자와 단위를 붙인다
-            else:  # 나머지자리일 때
-                str_result = units[i] + str_result  # 단위만 붙인다
-        else:  # 2~9일 때
-            str_result = str_num + units[i] + str_result  # 숫자와 단위를 붙인다
-    str_result = str_result.strip()  # 문자열 앞뒤 공백을 제거한다
-    if len(str_result) == 0:
-        return None
-    if not str_result[0].isnumeric():  # 앞이 숫자가 아닌 문자인 경우
-        str_result = '1' + str_result  # 1을 붙인다
-    return str_result + str_suffix  # 접미사를 붙인다
-
 class economy_ko(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -63,7 +22,7 @@ class economy_ko(commands.Cog):
             print("user folder exist")
         else:
             os.makedirs("./lib/economy/users")
-
+            
     #참여
     @commands.command()
     async def 참여(self, ctx):
@@ -161,7 +120,7 @@ class economy_ko(commands.Cog):
                     ws = wb.active
                     money = ws.cell(row=1, column=2).value
                     wb.close()
-                    embed = discord.Embed(title="KET", description="<@" + str(user.id) + ">님은 " + str(get_kor_amount_string(money)) + "<:ket:753449741186105375>을 가지고 계십니다!", color=0xeff0f1)
+                    embed = discord.Embed(title="KET", description="<@" + str(user.id) + ">님은 " + str(money) + "<:ket:753449741186105375>을 가지고 계십니다!", color=0xeff0f1)
                     await ctx.send(embed=embed)
                 else:
                     embed = discord.Embed(title="NO", description="유저가 ``케테르 경제``에 참여하지 않았어요..", color=0xeff0f1)
@@ -174,7 +133,8 @@ class economy_ko(commands.Cog):
                 ws = wb.active
                 money = ws.cell(row=1, column=2).value
                 wb.close()
-                embed = discord.Embed(title="KET", description="<@" + str(ctx.author.id) + "> " + str(get_kor_amount_string(money)) + "<:ket:753449741186105375>을 가지고 계십니다!", color=0xeff0f1)
+                embed = discord.Embed(title="KET", description="<@" + str(ctx.author.id) + "> " + str(
+                    money) + "<:ket:753449741186105375>을 가지고 계십니다!", color=0xeff0f1)
                 await ctx.send(embed=embed)
             else:
                 embed = discord.Embed(title="NO", description="먼저 ``.참여``를 입력해서 케테르 경제에 참여해주세요!", color=0xeff0f1)
