ebe0cda199a50dad67d188d814c22d92225960ee
Yeon <49362746+AngelusYeon@users.noreply.github.com>
2020-09-15 12:57:05 +0900

    Update economy_ko.py

--- cogs/economy_ko.py
+++ cogs/economy_ko.py
@@ -604,6 +604,7 @@ class economy_ko(commands.Cog):
                     prices.append(ws.cell(row=2, column=i).value)
             times = list(range(1, 101))
             plt.figure(figsize=(39, 18))
+            plt.rc('font', 16)
             if prices[0] < prices[99]:
                 plt.plot(times, prices, 'r-', drawstyle='steps-post')
             else:
