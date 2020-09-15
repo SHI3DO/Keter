4a2c7ed8bcb16253109c8911236f71f34a661a73
Shio <61821471+Shio7@users.noreply.github.com>
2020-09-15 09:27:50 +0900

    asdasd

--- cogs/autoupdate.py
+++ cogs/autoupdate.py
@@ -45,7 +45,35 @@ class Autoupdate(commands.Cog):
             await ctx.send(f"**{filename}.py** removed")
         else:
             await ctx.send(f"Can't find **{filename}.py**")
+            
+    @commands.command()
+    @commands.check(permissions.is_owner)
+    async def evsremove(self, ctx, filename: str):
+        if os.path.isfile('./evs/' + filename + ".py"):
+            os.remove('./evs/' + filename + ".py")
+            await ctx.send(f"**{filename}.py** removed")
+        else:
+            await ctx.send(f"Can't find **{filename}.py**")
 
+    @commands.command()
+    @commands.check(permissions.is_owner)
+    async def evsupdate(self, ctx, filename: str):
+        await ctx.trigger_typing()
+        await ctx.send("Updating source code...")
+        link = "https://raw.githubusercontent.com/Shio7/Keter/master/evs/" + filename + ".py"
+        r = requests.get(link, allow_redirects=True)
+        if os.path.isfile('./evs/' + filename + ".py"):
+            os.remove('./evs/' + filename + ".py")
+            open('./evs/' + filename + ".py", 'wb').write(r.content)
+        else:
+            open('./evs/' + filename + ".py", 'wb').write(r.content)
+        await ctx.send("Updated: "+filename+".py")
+        """ Loads an extension. """
+        try:
+            self.bot.load_extension(f"cogs.{filename}")
+        except Exception as e:
+            return await ctx.send(default.traceback_maker(e))
+        await ctx.send(f"Loaded evs **{filename}.py**")
 
 
 
