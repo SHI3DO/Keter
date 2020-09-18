f5b2714fc7b2c358ef8d67212052b4eab4db6535
Yeon <49362746+AngelusYeon@users.noreply.github.com>
2020-09-18 22:29:55 +0900

    Update economy_ko.py

--- cogs/economy_ko.py
+++ cogs/economy_ko.py
@@ -643,9 +643,9 @@ class economy_ko(commands.Cog):
                 next = float(ti) + 1800 - round(time.time())
                 embed = discord.Embed(title="NO", description="주식 거래 후 30분 동안은 추가 매매가 불가능 합니다.", color=0xeff0f1)
                 if next > 59:
-                    embed.set_footer(text="다음 주식 거래 허가까지 " + str(math.floor(next/60)) + "분 " + str((next/60 - math.floor(next/60))*60) + "초")
+                    embed.set_footer(text="다음 주식 거래 허가까지 " + str(math.floor(next/60)) + "분 " + str((round(next)/60 - math.floor(next/60))*60) + "초")
                 else:
-                    embed.set_footer(text="다음 주식 거래 허가까지 " + str(next) + "초")
+                    embed.set_footer(text="다음 주식 거래 허가까지 " + str(round(next)) + "초")
                 embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/750540820842807396/752684853320745000/KETER_PRESTIGE.png")
                 return await ctx.send(embed=embed)
             if block == 0:
