ee0703afda69586693703d65b8edd4da414a8f8c
Shio <61821471+Shio7@users.noreply.github.com>
2020-09-30 23:04:39 +0900

    Create shindan_ko.py

---
+++ cogs/shindan_ko.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from evs import default
+
+class Shindan_ko(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = default.get("config.json")
+
+    # Commands
+    @commands.command(name="진단")
+    async def _say(self, ctx, *, content:str):
+            await ctx.send("준비중")
+
+def setup(bot):
+    bot.add_cog(Shindan_ko(bot))
