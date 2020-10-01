b604e2d14c7c744b98cef56b9363e60c1c968778
Yeon <49362746+AngelusYeon@users.noreply.github.com>
2020-10-01 21:53:09 +0900

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -190,6 +190,10 @@ class Admin(commands.Cog):
     @commands.check(permissions.is_owner)
     async def change_version(self, ctx, ver: str):
         """ Change current version. """
+        if os.path.isdir("./lib/economy/cache"):
+            pass
+        else:
+            os.makedirs("./lib/economy/cache")
         f = open(cachelib + "version.ccf", "w")
         f.write(ver)
         f.close
