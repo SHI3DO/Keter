e6e9b26effd8694625e050d2d221888b7a28348b
Shio <61821471+Shio7@users.noreply.github.com>
2020-10-01 10:59:22 +0900

    Update shindan_ko.py

--- cogs/shindan_ko.py
+++ cogs/shindan_ko.py
@@ -11,6 +11,15 @@ class Shindan_ko(commands.Cog):
     @commands.command(name="진단")
     async def _say(self, ctx, *, content:str):
             await ctx.send(str + "에 대한 진단을 만드시겠습니까?")
+            def reaction_check_(m):
+            if m.message_id == msg.id and m.user_id == ctx.author.id and str(m.emoji) == "✅":
+                return True
+            return False
+
+        try:
+            await msg.add_reaction("✅")
+            await self.bot.wait_for('raw_reaction_add', timeout=10.0, check=reaction_check_)
+            
 
 def setup(bot):
     bot.add_cog(Shindan_ko(bot))
