47cb0f5bab9616956d04a9d8dd0f17e5624da747
Shio <61821471+Shio7@users.noreply.github.com>
2020-10-01 11:01:05 +0900

    Update shindan_ko.py

--- cogs/shindan_ko.py
+++ cogs/shindan_ko.py
@@ -19,6 +19,20 @@ class Shindan_ko(commands.Cog):
         try:
             await msg.add_reaction("✅")
             await self.bot.wait_for('raw_reaction_add', timeout=10.0, check=reaction_check_)
+
+
+        except asyncio.TimeoutError:
+            await msg.delete()
+            embed = discord.Embed(title="진단메이커", description="동의하지 않으셨습니다", color=0xeff0f1)
+            embed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/750540820842807396/752690012369190942/DARK_KETER_1.png")
+            await ctx.send(embed=embed)
+
+        except discord.Forbidden:
+            embed = discord.Embed(title="진단메이커", description="동의하지 않으셨습니다", color=0xeff0f1)
+            embed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/750540820842807396/752684853320745000/KETER_PRESTIGE.png")
+            await msg.edit(content=embed)
             
 
 def setup(bot):
