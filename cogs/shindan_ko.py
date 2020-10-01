a767c5e3bf8a0a77edddf37829a28bb7789dffdb
Shio <61821471+Shio7@users.noreply.github.com>
2020-10-01 10:54:06 +0900

    Update shindan_ko.py

--- cogs/shindan_ko.py
+++ cogs/shindan_ko.py
@@ -9,8 +9,8 @@ class Shindan_ko(commands.Cog):
 
     # Commands
     @commands.command(name="진단")
-    async def _say(self, ctx):
-            await ctx.send("준비중")
+    async def _say(self, ctx, *, content:str):
+            await ctx.send(str + "에 대한 진단을 만드시겠습니까?")
 
 def setup(bot):
     bot.add_cog(Shindan_ko(bot))
