ce28c2d6f84b9590892d87c511a81f2b093c4edb
Shio <61821471+Shio7@users.noreply.github.com>
2020-10-01 11:16:20 +0900

    Update shindan_ko.py

--- cogs/shindan_ko.py
+++ cogs/shindan_ko.py
@@ -22,7 +22,9 @@ class Shindan_ko(commands.Cog):
     # Commands
     @commands.command(name="진단")
     async def _say(self, ctx, *, content:str):
-            await ctx.send(content + "에 대한 진단을 만드시겠습니까?")
+            embed = discord.Embed(title="진단메이커", description=content + "에 대한 진단을 만드시겠습니까?", color=0xeff0f1)
+            await ctx.send(embed=embed)
+            msg = await ctx.send(embed=embed)
             def reaction_check_(m):
                 if m.message_id == msg.id and m.user_id == ctx.author.id and str(m.emoji) == "✅":
                     return True
