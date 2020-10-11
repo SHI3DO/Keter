9bfef33b916487196b1d4ec17537b501239d05d7
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-10-11 15:38:37 +0900

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -66,23 +66,8 @@ class Information(commands.Cog):
         await ctx.send(content=f"ℹ About **{ctx.bot.user}** | **" + version + "**", embed=embed)
 
     @commands.command()
-        async def serverinfo(self, ctx):
-            server = ctx.message.guild
-            channel_count = len([x for x in server.channels if type(x) == discord.channel.TextChannel])
-            channel__count = len([x for x in server.channels if type(x) == discord.channel.VoiceChannel])
-            embed = discord.Embed(title='Server Info',description='',color=0xFF4500)
-            embed.add_field(name='Name',value=server.name)
-            embed.add_field(name='Owner',value=server.owner,inline=True)
-            embed.add_field(name='Members',value=server.member_count)
-            embed.add_field(name='Region',value=server.region)
-            embed.add_field(name='Roles',value=str(len(server.roles)))
-            embed.add_field(name='Emojis',value=str(len(server.emojis)))
-            embed.add_field(name='Server ID',value=server.id)
-            embed.add_field(name='Text Channels',value=str(channel_count))
-            embed.add_field(name='Voice Channels',value=str(channel__count))
-            embed.add_field(name='Verification Level',value=str(server.verification_level))
-            embed.set_thumbnail(url=server.icon_url)
-            await ctx.send(embed=embed)
+    async def test(ctx):
+        await ctx.send('Hello')
 
 def setup(bot):
     bot.add_cog(Information(bot))
