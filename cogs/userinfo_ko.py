646401970ddf740dddb5d43836279a0d8e463c2a
RythmAviation <67253200+RythmAviation@users.noreply.github.com>
2020-10-11 16:29:27 +0900

    Update userinfo_ko.py

--- cogs/userinfo_ko.py
+++ cogs/userinfo_ko.py
@@ -12,36 +12,42 @@ class Userinfo_Ko(commands.Cog):
     async def _userinfo(self, ctx):
         if (ctx.message.mentions.__len__() > 0):
             for user in ctx.message.mentions:
-                embed = discord.Embed(title="**" + user.name + "**님의 프로필", description="",
+                embed = discord.Embed(title="**" + user.name + "**의 프로필", description="",
                                       color=0xeff0f1)
-                embed.add_field(name="**아이디**",
-                                value=user.id,
-                                inline=True)
-                embed.add_field(name="**별명**",
+                embed.add_field(name="**유저 이름**",
                                 value=user.display_name,
                                 inline=True)
                 embed.add_field(name="**상태**",
                                 value=user.status,
+                                inline=True)  
+                embed.add_field(name="**ID**",
+                                value=user.id,
                                 inline=True)
                 embed.add_field(name="**언급**",
                                 value="<@" + str(user.id) + ">",
                                 inline=True)
+                embed.add_field(name='**계정 생성일**',
+                                value=user.created_at.__format__('%A, %B %d %Y, %H:%M:%S'),
+                                inline=True)
                 embed.set_thumbnail(url=user.avatar_url)
                 await ctx.send(embed=embed)
         else:
-            embed = discord.Embed(title=ctx.author.name + "님의 프로필", description="",
+            embed = discord.Embed(title=ctx.author.name + "의 프로필", description="",
                                   color=0xeff0f1)
-            embed.add_field(name="**아이디**",
-                            value=ctx.author.id,
-                            inline=True)
-            embed.add_field(name="**별명**",
+            embed.add_field(name="**유저 이름**",
                             value=ctx.author.display_name,
                             inline=True)
             embed.add_field(name="**상태**",
                             value=ctx.author.status,
                             inline=True)
+            embed.add_field(name="**ID**",
+                            value=ctx.author.id,
+                            inline=True)
             embed.add_field(name="**언급**",
                             value="<@" + str(ctx.author.id) + ">",
+                            inline=True)
+            embed.add_field(name="**계정 생성일**",
+                            value=ctx.author.created_at.__format__('%A, %B %d %Y, %H:%M:%S'),
                             inline=True)
             embed.set_thumbnail(url=ctx.author.avatar_url)
             await ctx.send(embed=embed)
