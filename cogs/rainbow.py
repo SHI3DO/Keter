7f165c14ea58db054231e08295d5bdac28b6320c
Yeon <49362746+AngelusYeon@users.noreply.github.com>
2020-10-15 13:16:52 +0900

    Create rainbow.py

---
+++ cogs/rainbow.py
@@ -0,0 +1,66 @@
+import time
+import aiohttp
+import discord
+import importlib
+import os
+import sys
+
+from discord.ext import commands
+from evs import permissions, default, http, dataIO
+
+cachelib = "./lib/cache/"
+
+
+class Rainbow(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = default.get("config.json")
+
+    @commands.command()
+    @commands.check(permissions.is_owner)
+    async def rainbow(self, ctx, role:str):
+        for r in ctx.guild.roles:
+            if r.name == role:
+                print("detected role")
+                await ctx.send("detected role")
+                cycle = 0
+                while not ctx.is_closed():
+                    if cycle < 1791:
+                        cycle += 1
+                    else:
+                        cycle = 0
+                    if 0 <= cycle < 256:
+                        red = 255
+                        green = cycle
+                        blue = 0
+                    elif 256 <= cycle < 512:
+                        red = 512 - cycle
+                        green = 255
+                        blue = 0
+                    elif 512 <= cycle < 768:
+                        red = 0
+                        green = 255
+                        blue = cycle - 512
+                    elif 768 <= cycle < 1024:
+                        red = 0
+                        green = 1024 - cycle
+                        blue = 255
+                    elif 1280 <= cycle < 1536:
+                        red = 0
+                        green = 1024 - cycle
+                        blue = 255
+                    else:
+                        red = cycle - 1536
+                        green = 0
+                        blue = 1736 - cycle
+                    colour = 65536*red + 256*green + blue
+                    try:
+                        await role.edit(color=colour)
+                    except Exception:
+                        return await ctx.send("For some reason, I can no longer change the role ;-;")
+                    await asyncio.sleep(0.04)
+        return await ctx.send(f"role with the name {role} not found")
+        print(f"role with the name {role} not found")
+
+def setup(bot):
+    bot.add_cog(Rainbow(bot))
