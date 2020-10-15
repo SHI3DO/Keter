a0828a3fe6c904a10a46ea4d8e7fe95e491e9ef4
Yeon <49362746+AngelusYeon@users.noreply.github.com>
2020-10-15 15:11:17 +0900

    Update rainbow.py

--- cogs/rainbow.py
+++ cogs/rainbow.py
@@ -5,6 +5,37 @@ from discord.ext import commands
 from evs import permissions, default, http, dataIO
 
 cachelib = "./lib/cache/"
+colour = []
+def cring():
+    cycle = -1
+    while cycle <= 1534:
+        cycle += 1
+        if 0 <= cycle < 256:
+            red = 255
+            green = cycle
+            blue = 0
+        if 256 <= cycle < 512:
+            red = 511 - cycle
+            green = 255
+            blue = 0
+        if 512 <= cycle < 768:
+            red = 0
+            green = 255
+            blue = cycle - 512
+        if 768 <= cycle < 1024:
+            red = 0
+            green = 1023 - cycle
+            blue = 255
+        if 1024 <= cycle < 1280:
+            red = cycle - 1536
+            green = 0
+            blue = 255
+        if 1280 <= cycle < 1536:
+            red = 255
+            green = 0
+            blue = 1535 - cycle
+        colour.append(65536 * red + 256 * green + blue)
+cring()
 
 
 class Rainbow(commands.Cog):
@@ -19,41 +50,16 @@ class Rainbow(commands.Cog):
             if r.name == role:
                 print("detected role")
                 await ctx.send("detected role")
-                cycle = 0
+                cycle = -1
                 f = open(f"./lib/cache/{str(ctx.guild.id)}.ccf", "w")
                 f.close()
                 while os.path.isfile(f"./lib/cache/{str(ctx.guild.id)}.ccf"):
-                    if cycle < 1791:
+                    if cycle < 1536:
                         cycle += 1
                     else:
                         cycle = 0
-                    if 0 <= cycle < 256:
-                        red = 255
-                        green = cycle
-                        blue = 0
-                    if 256 <= cycle < 512:
-                        red = 511 - cycle
-                        green = 255
-                        blue = 0
-                    if 512 <= cycle < 768:
-                        red = 0
-                        green = 255
-                        blue = cycle - 512
-                    if 768 <= cycle < 1024:
-                        red = 0
-                        green = 1023 - cycle
-                        blue = 255
-                    if 1280 <= cycle < 1536:
-                        red = cycle - 1536
-                        green = 0
-                        blue = 255
-                    if 1537 <= cycle < 1792:
-                        red = 255
-                        green = 0
-                        blue = 1735 - cycle
-                    colour = 65536*red + 256*green + blue
                     try:
-                        await r.edit(color=discord.Colour(colour))
+                        await r.edit(color=discord.Colour(colour[cycle]))
                     except Exception:
                         return await ctx.send("For some reason, I can no longer change the role ;-;")
                     await asyncio.sleep(0.04)
