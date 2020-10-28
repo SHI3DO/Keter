23dc74a7aadd56de4332d2eae8662fc3be76abbe
Shio <61821471+Shio7@users.noreply.github.com>
2020-10-29 00:03:33 +0900

    Update covid19.py

--- cogs/covid19.py
+++ cogs/covid19.py
@@ -1,8 +1,14 @@
 import discord
-from discord.ext import commands
-from evs import default
+import time
 import psutil
 import os
+import asyncio
+import openpyxl
+import random
+import math
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
 
 class covid19(commands.Cog):
     def __init__(self, bot):
@@ -10,9 +16,16 @@ class covid19(commands.Cog):
         self.config = default.get("config.json")
         self.process = psutil.Process(os.getpid())
         
-    @commands.command
-    async def aaaaa(self, ctx):
-        await ctx.send("^^^^")
-
+    # Commands
+    @commands.command(name="말해에")
+    async def _say(self, ctx, *, content:str):
+        async with ctx.typing():
+            await ctx.message.delete()
+        if (content == "@everyone"):
+            embed = discord.Embed(title="안돼요", description='You cannot do that', color=0xeff0f1)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send(content)
+        
 def setup(bot):
     bot.add_cog(covid19(bot))
