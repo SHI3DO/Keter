9d80a86845c118fe9112862149cde6793861576b
Shio <61821471+Shio7@users.noreply.github.com>
2020-10-29 00:12:46 +0900

    Update covid19.py

--- cogs/covid19.py
+++ cogs/covid19.py
@@ -15,17 +15,50 @@ class covid19(commands.Cog):
         self.bot = bot
         self.config = default.get("config.json")
         self.process = psutil.Process(os.getpid())
-        
-    # Commands
-    @commands.command(name="말해에")
-    async def _say(self, ctx, *, content:str):
-        async with ctx.typing():
-            await ctx.message.delete()
-        if (content == "@everyone"):
-            embed = discord.Embed(title="안돼요", description='You cannot do that', color=0xeff0f1)
+
+    # 참여
+    @commands.command()
+    async def 약복용알리미(self, ctx):
+        embed = discord.Embed(title="약복용 알리미 시스템", description="약복용 알리미 시스템을 이용하시겠습니까?", color=0xeff0f1)
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/attachments/750540820842807396/752684853320745000/KETER_PRESTIGE.png")
+        msg = await ctx.send(embed=embed)
+
+        def reaction_check_(m):
+            if m.message_id == msg.id and m.user_id == ctx.author.id and str(m.emoji) == "✅":
+                return True
+            return False
+
+        try:
+            await msg.add_reaction("✅")
+            await self.bot.wait_for('raw_reaction_add', timeout=10.0, check=reaction_check_)
+            if os.path.isfile(userlib + str(ctx.author.id) + ".xlsx"):
+                embed = discord.Embed(title="케테르 경제", description="이미 참여하셨습니다.", color=0xeff0f1)
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(title="케테르 경제",
+                                      description="새로 오셨군요? " + str(ctx.author.name) + "님을 위한 파일들을 생성중이에요!",
+                                      color=0xeff0f1)
+                embed.set_thumbnail(
+                    url="https://cdn.discordapp.com/attachments/750540820842807396/752690012369190942/DARK_KETER_1.png")
+                await ctx.send(embed=embed)
+                embed = discord.Embed(title="케테르 경제",
+                                      description=str(ctx.author.name) + " 생성 완료!",
+                                      color=0xeff0f1)
+                await ctx.send(embed=embed)
+
+        except asyncio.TimeoutError:
+            await msg.delete()
+            embed = discord.Embed(title="케테르 경제", description="서명하지 않으셨습니다. 다음 기회에..", color=0xeff0f1)
+            embed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/750540820842807396/752690012369190942/DARK_KETER_1.png")
             await ctx.send(embed=embed)
-        else:
-            await ctx.send(content)
+
+        except discord.Forbidden:
+            embed = discord.Embed(title="케테르 경제", description="케테르 경제에 참여하시겠습니까?", color=0xeff0f1)
+            embed.set_thumbnail(
+                url="https://cdn.discordapp.com/attachments/750540820842807396/752684853320745000/KETER_PRESTIGE.png")
+            await msg.edit(content=embed)
         
 def setup(bot):
     bot.add_cog(covid19(bot))
