1cffd5fb6454103977b4651fcee5633643179b0a
Yeon <49362746+AngelusYeon@users.noreply.github.com>
2020-11-26 17:12:11 +0900

    Update shindan_ko.py

--- cogs/shindan_ko.py
+++ cogs/shindan_ko.py
@@ -560,7 +560,7 @@ class Shindan_ko(commands.Cog):
 
         try:
             await self.bot.wait_for('message', timeout=10.0, check=msg_check)
-            url = str(msg.attachments).split("url='")[1].replace("'>]", "")
+            await ctx.send(str(msg.attachments))
             try:
                 url = str(msg.attachments).split("url='")[1].replace("'>]", "")
             except:
