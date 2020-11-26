26a6f6918b243dc2e3ed3aa5689badd1766fa4e8
Yeon <49362746+AngelusYeon@users.noreply.github.com>
2020-11-26 16:28:10 +0900

    Update shindan_ko.py

--- cogs/shindan_ko.py
+++ cogs/shindan_ko.py
@@ -561,7 +561,7 @@ class Shindan_ko(commands.Cog):
         try:
             await self.bot.wait_for('message', timeout=10.0, check=msg_check)
             await msg.delete()
-            if len(msg.attachments.split("url='")) == 1 or len(msg.attachments.split("url='")) > 2:
+            if len(msg.attachments) == 0 or len(msg.attachments) > 1:
                 embed = discord.Embed(title="진단메이커", description="파일이 올바르지 않습니다.", color=0xeff0f1)
                 embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/750540820842807396/752690012369190942/DARK_KETER_1.png")
                 return await ctx.send(embed=embed)
