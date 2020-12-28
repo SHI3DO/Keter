b3b64929ea5cf390851aaeb6c785650a41f02934
Shio <shio7113@gmail.com>
2020-12-28 12:09:36 +0900

    yeoff

--- cogs/autoupdate.py
+++ cogs/autoupdate.py
@@ -110,8 +110,16 @@ class Autoupdate(commands.Cog):
             await ctx.trigger_typing()
             try:
                 r = requests.get(content[0], allow_redirects=True)
-                f = open(content[1] + content[2] + ".py", "r")
-                if f.read() == "404: Not Found":
+                if os.path.isfile(content[1] + content[2] + ".py"):
+                    embed = discord.Embed(title="관리모듈 A1",
+                                        description=content[2] + "모듈이 이미 있어요. `업데이트` 커맨드를 사용해 주세요.",
+                                        color=0xeff0f1)
+                    embed.set_footer(icon_url=ctx.author.avatar_url,
+                                         text=ctx.author.name + "#" + ctx.author.discriminator + " " + str(
+                                             datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
+                    msg = await ctx.send(embed=embed)
+
+                else:
                     await ctx.send("소스코드 다운로드 중...")
                     open(content[1] + content[2] + ".py", 'wb').write(r.content)
                     await ctx.send("Downloaded: " + content[2] + ".py")
@@ -121,27 +129,6 @@ class Autoupdate(commands.Cog):
                     except Exception as e:
                         return await ctx.send(default.traceback_maker(e))
                     await ctx.send(f"Loaded extension **{content[2]}.py**")
-
-                else:
-                    if os.path.isfile(content[1] + content[2] + ".py"):
-                        embed = discord.Embed(title="관리모듈 A1",
-                                              description=content[2] + "모듈이 이미 있어요. `업데이트` 커맨드를 사용해 주세요.",
-                                              color=0xeff0f1)
-                        embed.set_footer(icon_url=ctx.author.avatar_url,
-                                         text=ctx.author.name + "#" + ctx.author.discriminator + " " + str(
-                                             datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
-                        msg = await ctx.send(embed=embed)
-
-                    else:
-                        await ctx.send("소스코드 다운로드 중...")
-                        open(content[1] + content[2] + ".py", 'wb').write(r.content)
-                        await ctx.send("Downloaded: " + content[2] + ".py")
-                        """ Loads an extension. """
-                        try:
-                            self.bot.load_extension(f"cogs.{content[2]}")
-                        except Exception as e:
-                            return await ctx.send(default.traceback_maker(e))
-                        await ctx.send(f"Loaded extension **{content[2]}.py**")
 
             except:
                 embed = discord.Embed(title="관리모듈 A1", description="에러 발생!",
@@ -163,17 +150,53 @@ class Autoupdate(commands.Cog):
 
     @commands.command()
     @commands.check(permissions.is_owner)
-    async def 지우기(self, ctx, filename: str):
-        if os.path.isfile('./cogs/' + filename + ".py"):
-            try:
-                self.bot.unload_extension(f"cogs.{filename}")
-            except Exception as e:
-                return await ctx.send(default.traceback_maker(e))
-            await ctx.send(f"Unloaded extension **{filename}.py**")
-            os.remove('./cogs/' + filename + ".py")
-            await ctx.send(f"**{filename}.py** 삭제완료")
-        else:
-            await ctx.send(f"**{filename}.py 찾을 수 없음**")
+    async def 지우기(self, ctx, filename:str):
+        embed = discord.Embed(title="관리모듈 A1", description=filename + "모듈을 지우시겠습니까?", color=0xeff0f1)
+        embed.set_footer(icon_url=ctx.author.avatar_url,
+                         text=ctx.author.name + "#" + ctx.author.discriminator + " " + str(
+                             datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
+        msg = await ctx.send(embed=embed)
+
+        def reaction_check_(m):
+            if m.message_id == msg.id and m.user_id == ctx.author.id and str(m.emoji) == "✅":
+                return True
+            return False
+
+        try:
+            await msg.add_reaction("✅")
+            await self.bot.wait_for('raw_reaction_add', timeout=10.0, check=reaction_check_)
+            await ctx.trigger_typing()
+
+            if os.path.isfile('./cogs/' + filename + ".py"):
+                try:
+                    self.bot.unload_extension(f"cogs.{filename}")
+                except Exception as e:
+                    return await ctx.send(default.traceback_maker(e))
+                os.remove('./cogs/' + filename + ".py")
+                embed = discord.Embed(title="관리모듈 A1", description=filename + ".py 삭제 완료!",
+                                      color=0xeff0f1)
+                embed.set_footer(icon_url=ctx.author.avatar_url,
+                                 text=ctx.author.name + "#" + ctx.author.discriminator + " " + str(
+                                     datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
+                embed.set_thumbnail(url=parfait_url)
+                msg = await ctx.send(embed=embed)
+
+            else:
+                embed = discord.Embed(title="관리모듈 A1", description="에러 발생!",
+                                      color=0xeff0f1)
+                embed.set_footer(icon_url=ctx.author.avatar_url,
+                                 text=ctx.author.name + "#" + ctx.author.discriminator + " " + str(
+                                     datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
+                embed.set_thumbnail(url=parfait_url)
+                msg = await ctx.send(embed=embed)
+
+        except:
+            await msg.delete()
+            embed = discord.Embed(title="관리모듈 A1", description="동의하지 않으셨습니다.", color=0xeff0f1)
+            embed.set_footer(icon_url=ctx.author.avatar_url,
+                             text=ctx.author.name + "#" + ctx.author.discriminator + " " + str(
+                                 datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
+            await ctx.send(embed=embed)
             
 def setup(bot):
     bot.add_cog(Autoupdate(bot))
