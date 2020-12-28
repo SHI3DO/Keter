9ada2247f8fe37e51fb063418b56111b919ebdba
Shio <shio7113@gmail.com>
2020-12-28 11:50:55 +0900

    asd

--- cogs/autoupdate.py
+++ cogs/autoupdate.py
@@ -108,24 +108,49 @@ class Autoupdate(commands.Cog):
             await msg.add_reaction("✅")
             await self.bot.wait_for('raw_reaction_add', timeout=10.0, check=reaction_check_)
             await ctx.trigger_typing()
-            r = requests.get(content[0], allow_redirects=True)
-            if os.path.isfile(content[1]+content[2]+".py"):
-                embed = discord.Embed(title="관리모듈 A1", description=content[2] + "모듈이 이미 있어요. `업데이트` 커맨드를 사용해 주세요.", color=0xeff0f1)
+            try:
+                r = requests.get(content[0], allow_redirects=True)
+                f = open(content[1] + content[2] + ".py", "r")
+                if f.read() == "404: Not Found":
+                    await ctx.send("소스코드 다운로드 중...")
+                    open(content[1] + content[2] + ".py", 'wb').write(r.content)
+                    await ctx.send("Downloaded: " + content[2] + ".py")
+                    """ Loads an extension. """
+                    try:
+                        self.bot.load_extension(f"cogs.{content[2]}")
+                    except Exception as e:
+                        return await ctx.send(default.traceback_maker(e))
+                    await ctx.send(f"Loaded extension **{content[2]}.py**")
+
+                else:
+                    if os.path.isfile(content[1] + content[2] + ".py"):
+                        embed = discord.Embed(title="관리모듈 A1",
+                                              description=content[2] + "모듈이 이미 있어요. `업데이트` 커맨드를 사용해 주세요.",
+                                              color=0xeff0f1)
+                        embed.set_footer(icon_url=ctx.author.avatar_url,
+                                         text=ctx.author.name + "#" + ctx.author.discriminator + " " + str(
+                                             datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
+                        msg = await ctx.send(embed=embed)
+
+                    else:
+                        await ctx.send("소스코드 다운로드 중...")
+                        open(content[1] + content[2] + ".py", 'wb').write(r.content)
+                        await ctx.send("Downloaded: " + content[2] + ".py")
+                        """ Loads an extension. """
+                        try:
+                            self.bot.load_extension(f"cogs.{content[2]}")
+                        except Exception as e:
+                            return await ctx.send(default.traceback_maker(e))
+                        await ctx.send(f"Loaded extension **{content[2]}.py**")
+
+            except:
+                embed = discord.Embed(title="관리모듈 A1", description="에러 발생!",
+                                      color=0xeff0f1)
                 embed.set_footer(icon_url=ctx.author.avatar_url,
                                  text=ctx.author.name + "#" + ctx.author.discriminator + " " + str(
                                      datetime.today().strftime('%Y-%m-%d %H:%M:%S')))
+                embed.set_thumbnail(url=parfait_url)
                 msg = await ctx.send(embed=embed)
-
-            else:
-                await ctx.send("소스코드 다운로드 중...")
-                open(content[1]+content[2]+".py", 'wb').write(r.content)
-                await ctx.send("Downloaded: " + content[2] + ".py")
-                """ Loads an extension. """
-                try:
-                    self.bot.load_extension(f"cogs.{content[2]}")
-                except Exception as e:
-                    return await ctx.send(default.traceback_maker(e))
-                await ctx.send(f"Loaded extension **{content[2]}.py**")
 
         except:
             await msg.delete()
@@ -149,35 +174,6 @@ class Autoupdate(commands.Cog):
             await ctx.send(f"**{filename}.py** 삭제완료")
         else:
             await ctx.send(f"**{filename}.py 찾을 수 없음**")
-            
-    @commands.command()
-    @commands.check(permissions.is_owner)
-    async def allupdate(self, ctx):
-        await ctx.trigger_typing()
-        await ctx.send("Updating source code...")
-        for file in os.listdir("cogs"):
-            if file.endswith(".py"):
-                name = file[:-3]
-                link = "https://raw.githubusercontent.com/Shio7/Keter/master/cogs/" + name + ".py"
-                r = requests.get(link, allow_redirects=True)
-                if os.path.isfile('./cogs/' + name + ".py"):
-                    try:
-                        self.bot.unload_extension(f"cogs.{name}")
-                    except Exception as e:
-                        return await ctx.send(default.traceback_maker(e))
-                    await ctx.send(f"Unloaded extension **{name}.py**")
-                    os.remove('./cogs/' + name + ".py")
-                    open('./cogs/' + name + ".py", 'wb').write(r.content)
-                else:
-                    open('./cogs/' + name + ".py", 'wb').write(r.content)
-                await ctx.send("Updated: "+name+".py")
-
-                try:
-                    self.bot.load_extension(f"cogs.{name}")
-                except Exception as e:
-                    return await ctx.send(default.traceback_maker(e))
-                await ctx.send(f"Loaded extension **{name}.py**") 
-        await ctx.send("All modules updated")      
             
 def setup(bot):
     bot.add_cog(Autoupdate(bot))
