cd46fba8e9a358a698457b074d3cc3878fd9c789
Shio <shio7113@gmail.com>
2020-12-29 14:00:10 +0900

    a

--- cogs/logger.py
+++ cogs/logger.py
@@ -4,15 +4,19 @@
 
 import discord
 from discord.ext import commands
+from discord import Embed
 from discord.ext.commands import has_permissions, MissingPermissions
 from evs import default
 from evs import permissions, default, http, dataIO
 import requests
 import os
 from datetime import datetime
+from discord.ext.commands import Cog
 
 logfolder = "./lib/logs/"
 loadingurl = "https://cdn.discordapp.com/attachments/751791353779716099/793328911568076800/keterloading.gif"
+
+client = discord.Client()
 
 class Logger(commands.Cog):
     def __init__(self, bot):
@@ -49,9 +53,7 @@ class Logger(commands.Cog):
             await self.bot.wait_for('raw_reaction_add', timeout=10.0, check=reaction_check_)
             if os.path.isfile(logfolder+str(ctx.guild.id)+".ktx"):
                 print("로그파일 존재")
-
                 try:
-                    print("aaaaa")
                     logf = open(logfolder + str(ctx.guild.id) + ".ktx", "r")
                     embed = discord.Embed(title="관리",
                                           description="기존에 설정 되어 있는 " + "<#" + logf.read() + ">" + " 채널을 변경하시겠습니까?",
@@ -140,6 +142,51 @@ class Logger(commands.Cog):
             embed.set_thumbnail(url=ctx.guild.icon_url)
             await ctx.send(embed=embed)
 
+    @Cog.listener()
+    async def on_user_update(self, ctx, *, before, after):
+        if os.path.isfile(logfolder+str(ctx.guild.id)+".ktx"):
+            logf = open(logfolder + str(ctx.guild.id) + ".ktx", "r")
+            log_channel = logf.read()
+            logf.close()
+            if before.name != after.name:
+                embed = Embed(title="Username change",
+                              colour=after.colour,
+                              timestamp=datetime.utcnow())
+
+                fields = [("Before", before.name, False),
+                          ("After", after.name, False)]
+
+                for name, value, inline in fields:
+                    embed.add_field(name=name, value=value, inline=inline)
+
+                await log_channel.send(embed=embed)
+
+            if before.discriminator != after.discriminator:
+                embed = Embed(title="Discriminator change",
+                              colour=after.colour,
+                              timestamp=datetime.utcnow())
+
+                fields = [("Before", before.discriminator, False),
+                          ("After", after.discriminator, False)]
+
+                for name, value, inline in fields:
+                    embed.add_field(name=name, value=value, inline=inline)
+
+                await log_channel.send(embed=embed)
+
+            if before.avatar_url != after.avatar_url:
+                embed = Embed(title="Avatar change",
+                              description="New image is below, old to the right.",
+                              colour=log_channel.guild.get_member(after.id).colour,
+                              timestamp=datetime.utcnow())
+
+                embed.set_thumbnail(url=before.avatar_url)
+                embed.set_image(url=after.avatar_url)
+
+                await log_channel.send(embed=embed)
+
+        else:
+            print("로그 config 없음")
 
 def setup(bot):
     bot.add_cog(Logger(bot))
